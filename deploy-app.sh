25d0f0c4c77d4ba615b8868e224aa5dc42136807
akifkemalaksu <akifkemalaksu@gmail.com>
2024-04-10 00:53:06 +0300

    update deploy

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,19 +1,22 @@
-# Step 1: Build the Docker image
+# Step 1: Pull latest version of project
+git pull
+
+# Step 2: Build the Docker image
 docker build -t magescam .
 
-# Step 2: Create a temporary container from the image
+# Step 3: Create a temporary container from the image
 docker create --name magescam-container magescam
 
-# Step 3: Remove old files from the Nginx directory
+# Step 4: Remove old files from the Nginx directory
 rm -rf /var/www/magescam/*
 
-# Step 4: Copy the build files from the container to your Nginx directory
+# Step 5: Copy the build files from the container to your Nginx directory
 docker cp magescam-container:/app/dist/ /var/www/magescam/
 
-# Step 5: Remove the temporary container
+# Step 6: Remove the temporary container
 docker rm magescam-container
 
-# Step 6: (Optional) Reload or restart Nginx to apply the changes
+# Step 7: (Optional) Reload or restart Nginx to apply the changes
 nginx -s reload
 
 echo "Deployment completed successfully."
